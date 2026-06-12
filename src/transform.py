28521ed723de81eac989f57a6fbfbaefb9d4ba1e
niranjangunnal <66912577+niranjangunnal@users.noreply.github.com>
2026-06-12 13:50:42 +0530

    Delete src directory

--- src/transform.py
+++
@@ -1,5 +0,0 @@
-def transform_data(df):
-  df = df[['userId', 'id', 'title', 'body']]
-  df.columns = ['user_id', 'post_id', 'title', 'content']
-  df['title_length'] = df['title'].apply(len)
-  return df
